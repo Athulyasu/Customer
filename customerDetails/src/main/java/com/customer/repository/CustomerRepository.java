1491837016312ee830c701952076cd129a97b781
Athulya <suathulyasabari5@gmail.com>
2023-04-28 17:06:56 +0530

    added customer searching based on name

--- customerDetails/src/main/java/com/customer/repository/CustomerRepository.java
+++ customerDetails/src/main/java/com/customer/repository/CustomerRepository.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 
 public interface CustomerRepository extends CrudRepository<Customer, UUID>
 {
-    @Query("SELECT C FROM Customer C where C.name LIKE %:customer_name% order by C.name")
+    @Query("SELECT C FROM Customer C where lower(C.name) LIKE lower(concat('%', :customer_name,'%')) order by C.name")
     List<Customer> searchCustomers(@Param("customer_name") String customer_name);
 
 }
